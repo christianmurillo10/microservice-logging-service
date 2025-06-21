cbdd98f0be6fadd3d88f5ec72b3badd3cf2034d9
Christian Murillo <christianoroceomurillo@gmail.com>
2025-06-21 23:32:36 +0800

    Implemented service based evenet listener for role module

--- src/events/consumer/role.consumer.ts
+++ src/events/consumer/role.consumer.ts
@@ -1,36 +1,5 @@
 import { KafkaMessage } from "kafkajs";
-import {
-  EVENT_ROLE_BULK_DELETED,
-  EVENT_ROLE_CREATED,
-  EVENT_ROLE_DELETED,
-  EVENT_ROLE_UPDATED
-} from "../../shared/constants/events.constant";
-import { ActionValue, EventMessageData } from "../../shared/types/common.type";
-import LoggingService, { Header } from "../../services/logging.service";
-
-const executeLoggingService = async (
-  action: ActionValue,
-  event_type: string,
-  user_id: string,
-  data: EventMessageData<any>,
-  header: Header
-) => {
-  const loggingService = new LoggingService({
-    service_name: "USER_SERVICE",
-    action: action,
-    event_type: event_type,
-    table_name: "roles",
-    table_id: data.new_details.id,
-    payload: data,
-    header: {
-      ip_address: header.ip_address,
-      user_agent: header.user_agent
-    },
-    user_id,
-    business_id: data.new_details.business_id ?? undefined
-  });
-  await loggingService.execute();
-};
+import RoleEventListenerService from "../../services/eventListener/role-event-listener.service";
 
 const roleConsumer = async (message: KafkaMessage) => {
   const userId = message.key?.toString() ?? "";
@@ -40,42 +9,17 @@ const roleConsumer = async (message: KafkaMessage) => {
     return;
   };
 
-  const header = {
-    ip_address: message.headers!.ip_address!.toString(),
-    user_agent: message.headers!.user_agent!.toString()
-  };
-  const eventType = value.eventType;
-
-  switch (eventType) {
-    case EVENT_ROLE_CREATED:
-      await executeLoggingService("CREATE", eventType, userId, value.data, header);
-      break;
-    case EVENT_ROLE_UPDATED:
-      await executeLoggingService("UPDATE", eventType, userId, value.data, header);
-      break;
-    case EVENT_ROLE_DELETED:
-      await executeLoggingService("DELETE", eventType, userId, value.data, header);
-      break;
-    case EVENT_ROLE_BULK_DELETED:
-      const roleIds = value.data.new_details.ids;
-
-      for (const roleId of roleIds) {
-        const data = {
-          old_details: {
-            id: roleId,
-            deleted_at: null,
-          },
-          new_details: {
-            id: roleId,
-            deleted_at: new Date(),
-          }
-        }
-        await executeLoggingService("DELETE_MANY", eventType, userId, data, header);
-      }
-      break;
-  };
-
-  console.info(`Event Notification: Successfully logged role updates.`);
+  const intance = new RoleEventListenerService();
+  intance.setState({
+    eventType: value.eventType,
+    userId,
+    value: value.data,
+    header: {
+      ip_address: message.headers!.ip_address!.toString(),
+      user_agent: message.headers!.user_agent!.toString()
+    }
+  });
+  await intance.execute();
 };
 
 export default roleConsumer;
--- src/services/eventListener/role-event-listener.service.ts
+++ src/services/eventListener/role-event-listener.service.ts
@@ -2,9 +2,14 @@ import EventListenerAbstract from "./event-listener.abstract";
 import EventListenerService from "./event-listener.interface";
 import { ActionValue, EventMessageData } from "../../shared/types/common.type";
 import LoggingService, { Header } from "../logging.service";
+import { EVENT_ROLE_BULK_DELETED, EVENT_ROLE_CREATED, EVENT_ROLE_DELETED, EVENT_ROLE_UPDATED } from "../../shared/constants/events.constant";
 
-export default class RoleEventListenerService extends EventListenerAbstract<unknown> implements EventListenerService<unknown> {
-  executeLoggingService = async (
+export default class RoleEventListenerService extends EventListenerAbstract<any> implements EventListenerService<any> {
+  constructor() {
+    super();
+  };
+
+  private executeLoggingService = async (
     action: ActionValue,
     event_type: string,
     user_id: string,
@@ -29,6 +34,42 @@ export default class RoleEventListenerService extends EventListenerAbstract<unkn
   };
 
   execute = async (): Promise<void> => {
+    if (!this.state) {
+      console.error("State cannot be empty!");
+      return;
+    };
 
+    const { eventType, userId, value, header } = this.state;
+
+    switch (eventType) {
+      case EVENT_ROLE_CREATED:
+        await this.executeLoggingService("CREATE", eventType, userId, value, header);
+        break;
+      case EVENT_ROLE_UPDATED:
+        await this.executeLoggingService("UPDATE", eventType, userId, value, header);
+        break;
+      case EVENT_ROLE_DELETED:
+        await this.executeLoggingService("DELETE", eventType, userId, value, header);
+        break;
+      case EVENT_ROLE_BULK_DELETED:
+        const roleIds = value.new_details.ids;
+
+        for (const roleId of roleIds) {
+          const data = {
+            old_details: {
+              id: roleId,
+              deleted_at: null,
+            },
+            new_details: {
+              id: roleId,
+              deleted_at: new Date(),
+            }
+          }
+          await this.executeLoggingService("DELETE_MANY", eventType, userId, data, header);
+        }
+        break;
+    };
+
+    console.info(`Event Notification: Successfully logged role updates.`);
   };
 };
